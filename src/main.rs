9a0b504c1bc4378608c86e66f39f5d26ce69f74e
emincanozcan <50255411+emincanozcan@users.noreply.github.com>
2023-07-05 01:00:04 +0300

    Hello Actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+async fn index() -> &'static str {
+    "Hello Actix!"
 }
